124ccec55750b213a1a32f07fe8f99de24442525
Chris Nikis <snein89@gmail.com>
2026-03-09 11:36:44 +0200

    initialize taiwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import "./App.css"; // Ensure this file has @import "tailwindcss"; inside it
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-600">
+        Tailwind v4 is Live!
+      </h1>
+    </div>
+  );
 }
 
 export default App;
